ad48d8e86f443cf42f9c8051eb7182bd98d104f9
Micah <contact_micah@yahoo.com>
2024-08-02 14:30:17 -0700

    Trying to send a photo - 5

--- src/components/MyWebCam2.js
+++ src/components/MyWebCam2.js
@@ -68,7 +68,7 @@ function Photo_capture_from_scratch() {
   // For caching: 
   const timestamp = Date.now();
   formData2.append("file", capturedFile, `${timestamp}_${capturedFile.name}`);  
-  console.log( "Hi ", formData2 )
+  //console.log( "Hi ", formData2 )
   const requestOptions = {
     method: "POST",
     body: formData2,
@@ -77,7 +77,9 @@ function Photo_capture_from_scratch() {
   // Aug 02, 24: sending a photo
   
   try {
-    const response = await fetch('https://0kl0o417d5.execute-api.us-west-2.amazonaws.com/dev/picture', {
+    console.log("Hello", formData2);
+    const response = await fetch(`https://0kl0o417d5.execute-api.us-west-2.amazonaws.com/dev/picture/${formData2}`, {
+    //const response = await fetch('https://0kl0o417d5.execute-api.us-west-2.amazonaws.com/dev/picture', {
       method: 'POST',
       body: formData2,
       // Don't set Content-Type header, let the browser set it with the correct boundary for FormData
